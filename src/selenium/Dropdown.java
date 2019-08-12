edec651fad54d6a333554265023cd60a08d42ec0
Michelle Garcia <thelionpear@gmail.com>
2019-08-12 14:34:04 -0600

    end to end script

--- src/selenium/Dropdown.java
+++ src/selenium/Dropdown.java
@@ -29,9 +29,7 @@ public class Dropdown {
 		//driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 		//after the above, need to select dates on the calendar or it gets an "click intercepted" error
-		
-		
-		
+		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active")).click();
 //		Select s = new Select(driver.findElement(By.id("divpaxinfo")));
 //		the dropdown has changed so these below don't work. They would work if the selections
 //		were in a list, but each selection has a + and - now. They are all divs. 
@@ -65,6 +63,20 @@ public class Dropdown {
 		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 		
+		// System.out.println(driver.findElement(By.name("ctl00$mainContent$view_date2")).isEnabled());
+		//the above doesn't work because it isn't enabled or disabled, it's just made to look
+		//disabled with html 
+		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
+		if(driver.findElement(By.id("Div1")).getAttribute("style").contains("1"))
+		{
+			System.out.println("its enabled");
+			Assert.assertTrue(true);
+
+		} else {
+			Assert.assertTrue(false);
+		}
 	}
 
 }
