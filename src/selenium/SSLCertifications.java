66e1bc66dc8a3b00de9580d9c66a66d4ae6f3da5
Michelle Garcia <thelionpear@gmail.com>
2019-08-24 22:54:22 -0600

    code for how to handle SSL Certificatins in Selenium

---
+++ src/selenium/SSLCertifications.java
@@ -0,0 +1,27 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class SSLCertifications {
+
+	public static void main(String[] args) {
+		//SSl certificates
+
+		//Desired capabilities=
+		//general chrome profile
+		DesiredCapabilities ch=DesiredCapabilities.chrome();
+		//ch.acceptInsecureCerts();
+		ch.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+		ch.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
+		//Belows to your local browser
+		ChromeOptions c= new ChromeOptions();
+		c.merge(ch);
+		System.setProperty("webdriver.chrome.driver", "");
+		WebDriver driver=new ChromeDriver(c);
+	}
+}
